80c47594ced895b1982601ee6fe86c11d5b87e30
Alexsandra-m-silva <142628895+Alexsandra-m-silva@users.noreply.github.com>
2025-01-14 07:13:41 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,7 @@ export const Hero = () => {
                 target="_blank"
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
-                  Learn How We're Changing Eye Health
+                  Learn How We are Changing Eye Health
               </a>
              
             </div>
